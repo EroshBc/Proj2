61d75aeb3493acf3bc45a9899069a5047f3c35ac
Erosh Boralugodage <erosh.bc@gmail.com>
2023-02-15 23:56:04 -0600

    Initial Commit

---
+++ Proj2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Proj2
+//
+//  Created by Erosh Boralugodage on 2/15/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
