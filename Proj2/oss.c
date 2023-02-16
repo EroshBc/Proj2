2a6fa5379e713e4bfb119639306e40b8b404bed8
Erosh Boralugodage <erosh.bc@gmail.com>
2023-02-15 23:58:44 -0600

    Commit- create oss and worker

---
+++ Proj2/oss.c
@@ -0,0 +1,8 @@
+//
+//  oss.c
+//  Proj2
+//
+//  Created by Erosh Boralugodage on 2/15/23.
+//
+
+#include <stdio.h>
---
+++ Proj2/worker.c
@@ -0,0 +1,8 @@
+//
+//  worker.c
+//  Proj2
+//
+//  Created by Erosh Boralugodage on 2/15/23.
+//
+
+#include <stdio.h>
